375927f289d4525c4be310f0197dc90bfa32d26f
Nadi-Costanzo <ngpotutkina@yandex.ru>
2026-09-09 06:09:44 +0200

    feat: configure application logging

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,5 @@
-# Словарь с соглашениями для автогенерации имен
+from pathlib import Path
+
 NAMING_CONVENTION = {
     'ix': 'ix_%(column_0_label)s',
     'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
@@ -6,6 +7,7 @@ NAMING_CONVENTION = {
     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
     'pk': 'pk_%(table_name)s',
 }
+"""Словарь с соглашениями для автогенерации имен"""
 
 DB_CHECK_TIMEOUT_S = 2.0
 """Время ожидания проверки доступности базы данных в секундах."""
@@ -16,3 +18,20 @@ DB_DETAIL_TIMEOUT = (
 )
 DB_DETAIL_LOCKED = 'База данных заблокирована другим процессом.'
 DB_DETAIL_UNAVAILABLE = 'База данных недоступна.'
+
+
+# Конфигурация логирования
+LOG_BACKUP_COUNT = 3
+"""Максимальное число хранимых старых файлов логирования."""
+
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
+"""Формат даты и времени для логгера."""
+
+LOG_FILE_PATH = Path('logs/app.log')
+"""Путь к логам."""
+
+LOG_FORMAT = '%(asctime)s | %(levelname)s | %(name)s | %(message)s'
+"""Формат логирования."""
+
+LOG_MAX_BYTES = 30 * 1024 * 1024
+"""Максимальный размер файла логов в байтах."""
--- app/main.py
+++ app/main.py
@@ -6,14 +6,25 @@ from fastapi import FastAPI
 from app.api.health import router as health_router
 from app.config import settings
 from app.database import async_engine, initialize_database
+from app.logging_config import get_logger, setup_logging
+
+setup_logging()
+logger = get_logger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
     """Управляет ресурсами приложения при его запуске и остановке."""
+    logger.info('Запуск приложения...')
     try:
+        logger.info('Инициализация БД.')
         await initialize_database(async_engine)
+        logger.info('БД успешно инициализирована.')
         yield
+        logger.info('Завершение работы приложения.')
+    except Exception:
+        logger.exception('Приложение не запущено: ошибка инициализации БД.')
+        raise
     finally:
         await async_engine.dispose()
 
